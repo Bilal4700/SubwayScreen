ee67baaf05eac262e9bab32dfbc06c44e3508ac7
Muhammad Bilal <bilal119797@gmail.com>
2024-08-05 15:58:44 -0600

    Forgot to save

--- Subway Screen/src/ca/ucalgary/ensf380/components/Fetcher.java
+++ Subway Screen/src/ca/ucalgary/ensf380/components/Fetcher.java
@@ -1,5 +1,7 @@
 package ca.ucalgary.ensf380.components;
 
+//Make it compulsory for every inheritant class to fetch the data from api
+
 abstract class Fetcher {
 
 	abstract void fetch() throws Exception;
